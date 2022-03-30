6d8f02435e7b4dcf58f32f2fc1ce962dae519c23
gdlbo <41114720+gdlbo@users.noreply.github.com>
2022-03-30 17:01:50 +0300

    Improvements of multiacc + deleted old accs xD

--- app/src/main/java/ru/vtosters/lite/ui/fragments/AccountsFragment.java
+++ app/src/main/java/ru/vtosters/lite/ui/fragments/AccountsFragment.java
@@ -1,9 +1,11 @@
 package ru.vtosters.lite.ui.fragments;
 
+import static android.content.Context.MODE_PRIVATE;
 import static ru.vtosters.lite.utils.Helper.GetContext;
 import static ru.vtosters.lite.utils.Helper.GetPreferences;
 import static ru.vtosters.lite.utils.Helper.getUserLastName;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.preference.Preference;
 
@@ -18,6 +20,7 @@ import com.vtosters.lite.auth.VKAccountManager;
 import com.vtosters.lite.fragments.MaterialPreferenceToolbarFragment;
 import com.vtosters.lite.im.ImEngineProvider;
 
+import ru.vtosters.lite.tgs.TGPref;
 import ru.vtosters.lite.tgs.TGRoot;
 import ru.vtosters.lite.ui.PreferencesUtil;
 import ru.vtosters.lite.utils.AccountManager;
@@ -29,15 +32,17 @@ public class AccountsFragment extends MaterialPreferenceToolbarFragment {
     public void b(Bundle bundle) {
         super.b(bundle);
 
+        ToastUtils.a(AccountManager.getAvalibleAcc());
+
         int vtosterXml = Helper.GetContext().getResources().getIdentifier("empty", "xml", Helper.GetContext().getPackageName());
         this.a(vtosterXml);
 
         PreferencesUtil.addPreferenceCategory(this, "Аккаунты");
 
         PreferencesUtil.addListPreference(this, "account", "", "Выберите аккаунт", new CharSequence[]{
-                AccountManager.getName(""), AccountManager.getName("1")
+                AccountManager.getName("0"), AccountManager.getName("1")
         }, new String[]{
-                "", "1"
+                "0", "1"
         });
 
         a("account").b(AccountManager.getCurrentAccountName());
@@ -50,11 +55,27 @@ public class AccountsFragment extends MaterialPreferenceToolbarFragment {
             VKAccount b = VKAccountManager.b();
             PushSubscriber.a.a(b.b(), b.c());
             AppContextHolder.a.getSharedPreferences("gcm", 0).edit().clear().apply();
-            AccountsFragment.this.aB().postDelayed(() -> {
-                PushSubscriber.a.a(true);
-            }, 1000);
+            ToastUtils.a("Перезапустите приложение");
             return true;
         });
+
+        PreferencesUtil.addPreference(this, "newacc", "Добавить аккаунт", "", null, preference -> {
+            SharedPreferences sharedPrefs = GetContext().getSharedPreferences("com.vtosters.lite_preferences", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPrefs.edit();
+            editor.putString("account", Integer.toString(AccountManager.getAvalibleAccInt() + 1));
+            editor.commit();
+
+            VKImageLoader.b();
+            ImEngineProvider.a().h();
+            AudioMessageUtils.j();
+            FileUtils.l();
+            VKAccount b = VKAccountManager.b();
+            PushSubscriber.a.a(b.b(), b.c());
+            AppContextHolder.a.getSharedPreferences("gcm", 0).edit().clear().apply();
+            ToastUtils.a("Перезапустите приложение");
+            return false;
+        });
+
     }
 
     @Override
