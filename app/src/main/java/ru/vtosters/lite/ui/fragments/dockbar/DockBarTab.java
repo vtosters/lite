f5382e96497cdecda218c8effafce914e404a7ee
Vologhat <planeta.play@yahoo.com>
2022-04-02 11:53:43 +0300

    migration to android support due to androidx instability

--- app/src/main/java/ru/vtosters/lite/ui/fragments/dockbar/DockBarTab.java
+++ app/src/main/java/ru/vtosters/lite/ui/fragments/dockbar/DockBarTab.java
@@ -1,18 +1,20 @@
 package ru.vtosters.lite.ui.fragments.dockbar;
 
+import com.vk.core.fragments.FragmentImpl;
+
 public class DockBarTab {
 
     public String tag;
     public int iconID;
     public int titleID;
-    public int resID;
-    public Class<?> fragmentClass;
+    public int id;
+    public Class<? extends FragmentImpl> fragmentClass;
 
     public DockBarTab(String tag, int iconID, int titleID, int resID, Class<?> fragmentClass) {
         this.tag = tag;
         this.iconID = iconID;
         this.titleID = titleID;
-        this.resID = resID;
-        this.fragmentClass = fragmentClass;
+        this.id = resID;
+        this.fragmentClass = (Class<? extends FragmentImpl>) fragmentClass;
     }
 }
