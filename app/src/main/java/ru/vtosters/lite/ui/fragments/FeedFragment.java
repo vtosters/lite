06ce8a7e6f4ec25e2d7c3b4ef418706f3bf4cd48
gdlbo <41114720+gdlbo@users.noreply.github.com>
2022-06-07 11:56:23 +0300

    New feature open news settings via settings lmao

--- app/src/main/java/ru/vtosters/lite/ui/fragments/FeedFragment.java
+++ app/src/main/java/ru/vtosters/lite/ui/fragments/FeedFragment.java
@@ -1,7 +1,14 @@
 package ru.vtosters.lite.ui.fragments;
 
-import android.os.Bundle;
+import static ru.vtosters.lite.utils.Globals.getContext;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+
+import com.vk.navigation.Navigator;
+import com.vk.newsfeed.NewsfeedSettingsFragment;
 import com.vtosters.lite.R;
 import com.vtosters.lite.fragments.MaterialPreferenceToolbarFragment;
 
@@ -10,5 +17,23 @@ public class FeedFragment extends MaterialPreferenceToolbarFragment {
     public void b(Bundle bundle) {
         super.b(bundle);
         a(R.xml.preferences_feed);
+        prefs();
+    }
+
+    private void prefs() {
+        a("officialnewssett").a(new FeedFragment.openofficialsett());
+    }
+
+    public class openofficialsett implements Preference.c {
+        openofficialsett() {
+        }
+
+        @Override // android.support.v7.preference.Preference.c
+        public boolean a(Preference preference) {
+            Context context = getContext();
+            Intent a2 = new Navigator(NewsfeedSettingsFragment.class).a(context);
+            context.startActivity(a2);
+            return true;
+        }
     }
 }
