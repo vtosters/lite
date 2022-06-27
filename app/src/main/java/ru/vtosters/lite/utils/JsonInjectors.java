58ec32be6f46564675973081bc833469111e00f6
gdlbo <41114720+gdlbo@users.noreply.github.com>
2022-06-27 17:58:30 +0300

    covbar inj test 2

--- app/src/main/java/ru/vtosters/lite/utils/JsonInjectors.java
+++ app/src/main/java/ru/vtosters/lite/utils/JsonInjectors.java
@@ -16,15 +16,17 @@ public class JsonInjectors{
     public static JSONObject profileButton(JSONObject orig) throws JSONException{
         if(haveDonateButton()) return orig;
 
-        var pic = "https:\\/\\/sun1-18.userapi.com\\/NLd_rNpGuSaBnPV6O-j5mqCGZk8BK8drAMd2LQ\\/5R-DEF37PFs.png";
+        var pic = "https://sun1-18.userapi.com/NLd_rNpGuSaBnPV6O-j5mqCGZk8BK8drAMd2LQ/5R-DEF37PFs.png";
         var title = "Помоги проекту донатом и получи бонус!";
-        var link = "https:\\/\\/vk.com\\/vtosters_official";
+        var link = "https://vk.com/vtosters_official";
         var text_color = "2D81E0";
 
         return new JSONObject(decode("eyJhY3Rpb24iOnsidGFyZ2V0IjoiaW50ZXJuYWwiLCJ0eXBlIjoib3Blbl91cmwiLCJ1cmwiOiI=") + link + decode("In0sInRpdGxlIjoi") + title + decode("IiwiaWNvbnMiOlt7InVybCI6Ig==") + pic + decode("Iiwid2lkdGgiOjIwLCJoZWlnaHQiOjIwfSx7InVybCI6Ig==") + pic + decode("Iiwid2lkdGgiOjQwLCJoZWlnaHQiOjQwfSx7InVybCI6Ig==") + pic + decode("Iiwid2lkdGgiOjYwLCJoZWlnaHQiOjYwfSx7InVybCI6Ig==") + pic + decode("Iiwid2lkdGgiOjgwLCJoZWlnaHQiOjgwfV0sInRleHRfY29sb3IiOiI=") + text_color + decode("In0="));
     }
 
     public static JSONObject convBar(JSONObject orig) throws JSONException{
+        var pic = "https://nyaa.shikimori.one/system/users/x160/277870.png";
+
         var textverif = "Я не загрузил данные (9(9((";
 
         var peerid = Objects.requireNonNull(orig.optJSONObject("peer")).optInt("id");
@@ -37,7 +39,7 @@ public class JsonInjectors{
 
         if(!isVerified(peerid)) return orig.optJSONObject("conversation_bar");
 
-        return new JSONObject("{\"name\":\"group_admin_welcome\",\"text\":\"" + textverif + "\",\"buttons\":[]}");
+        return new JSONObject("{\"name\":\"group_admin_welcome\",\"text\":\"" + textverif + "\",\"buttons\":[],\"icon\":\"" + pic + "\"}");
     }
 
     public static boolean haveDonateButton(){
