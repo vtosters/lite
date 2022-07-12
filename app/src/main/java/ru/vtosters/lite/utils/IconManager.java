e144390d4fc88208ec0ace49fadadd800fdaec7b
Vologhat <47892605+Vologhat@users.noreply.github.com>
2022-07-12 23:08:02 +0300

    refactored icon switcher

--- app/src/main/java/ru/vtosters/lite/utils/IconManager.java
+++ app/src/main/java/ru/vtosters/lite/utils/IconManager.java
@@ -1,86 +1,98 @@
 package ru.vtosters.lite.utils;
-import static ru.vtosters.lite.utils.Globals.componentSwitcher;
+
 import static ru.vtosters.lite.utils.Globals.edit;
+import static ru.vtosters.lite.utils.Globals.getContext;
+import static ru.vtosters.lite.utils.Globals.getPackageName;
 import static ru.vtosters.lite.utils.Globals.getPreferences;
-import static ru.vtosters.lite.utils.Globals.sendToast;
 import static ru.vtosters.lite.utils.Preferences.hasVerification;
 import static ru.vtosters.lite.utils.Preferences.isValidSignature;
 
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class IconManager{
-    public static List<String> icons = Arrays.asList(
-            "standard", "vt" // available icons list
+public class IconManager {
+
+    // available icons list
+    public static List<String> sIcons = Arrays.asList(
+            "standard", "vt"
     );
 
-    public static List<String> appnames = Arrays.asList(
-            "standard", "vt", "vkontakte" // available appnames list
+    // available labels list
+    public static List<String> sLabels = Arrays.asList(
+            "standard", "vt", "vkontakte"
     );
 
-    public static List<String> iconsPlus = Arrays.asList(
-            "standard", "vt", "navy", "tiger", "spring", "sea", "sakura", "party", "paint", "flamingo" // available icons list for verified users
+    // available icons list for verified users
+    public static List<String> sIconsPlus = Arrays.asList(
+            "standard", "vt", "navy", "tiger", "spring", "sea", "sakura", "party", "paint", "flamingo"
     );
 
-    public static List<String> appnamesPlus = Arrays.asList(
-            "standard", "vt", "vkontakte" // available appnames list for verified users
+    // available labels list for verified users
+    public static List<String> sLabelsPlus = Arrays.asList(
+            "standard", "vt", "vkontakte"
     );
 
     public static CharSequence[] icons() {
-        var iconsList = icons;
-
-        if (hasVerification() && isValidSignature()) {
-            iconsList = iconsPlus;
-        }
-
-        return iconsList.toArray(new CharSequence[0]);
+        return (hasVerification() && isValidSignature() ? sIconsPlus : sIcons).toArray(new CharSequence[0]);
     }
 
     public static CharSequence[] iconsValues() {
-        var iconsList = icons;
-
-
-        if (hasVerification() && isValidSignature()) {
-            iconsList = iconsPlus;
-        }
-
-        return iconsList.toArray(new CharSequence[0]);
+        return (hasVerification() && isValidSignature() ? sIconsPlus : sIcons).toArray(new CharSequence[0]);
     }
 
-    public static void iconSwitcher(String icon, String appname){
-        var iconComponent = 1; // def value for standard icon
-        var appNameComponent = 1; // def value for standard appname
+    public static void switchComponent(String icon, String appName) {
+        // def value for standard icon
+        var iconIndex = 1;
+        // def value for standard label
+        var labelIndex = 1;
 
-        Log.d("IconManager", "iconSwitcher: icon = " + icon + ", appname = " + appname);
+        Log.d("IconManager", "iconSwitcher: icon = " + icon + ", appname = " + appName);
 
-        var iconsList = icons;
-        var appnamesList = appnames;
+        List<String> iconsList;
+        List<String> labelsList;
 
         if (hasVerification() && isValidSignature()) {
-            iconsList = iconsPlus;
-            appnamesList = appnamesPlus;
+            iconsList = sIconsPlus;
+            labelsList = sLabelsPlus;
+        } else {
+            iconsList = sIcons;
+            labelsList = sLabels;
         }
 
-        if (iconsList.contains(icon)) { // check if icon is exist
-            iconComponent = iconsList.indexOf(icon); // get icon index
-        }
+        // check if icon is exist
+        if (iconsList.contains(icon))
+            // get icon index
+            iconIndex = iconsList.indexOf(icon);
 
-        if (appnamesList.contains(appname)) { // check if appname is exist
-            appNameComponent = appnamesList.indexOf(appname); // get appname index
-        }
+        // check if labels is exist
+        if (labelsList.contains(appName))
+            // get labels index
+            labelIndex = labelsList.indexOf(appName);
 
-        String oldint = getPreferences().getString("components_enabled", "11");
-        String newint = iconComponent + "" + appNameComponent;
+        String currentState = getPreferences().getString("components_enabled", "11");
+        String newState = iconIndex + "" + labelIndex;
 
-        if (oldint == newint)
-            return; // check if current component is equal to custom value or if custom value is not exist
+        // check if current component is equal to custom value or if custom value is not exist
+        if (currentState.equals(newState))
+            return;
 
-        componentSwitcher("id" + oldint, false); // disable current component to prevent conflict with custom value
+        // disable current component to prevent conflict with custom value
+        switchIcon("id" + currentState, false);
+        // save custom value
+        edit().putString("components_enabled", newState).commit();
+        // enable custom value
+        switchIcon("id" + newState, true);
+    }
 
-        edit().putString("components_enabled", newint).commit(); // save custom value
-
-        componentSwitcher("id" + newint, true); // enable custom value
+    // Component switcher for changing app icon
+    public static void switchIcon(String componentName, Boolean enabled) {
+        getContext().getPackageManager().setComponentEnabledSetting(
+                new ComponentName(getPackageName(), getPackageName() + "." + componentName),
+                enabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
     }
 }
