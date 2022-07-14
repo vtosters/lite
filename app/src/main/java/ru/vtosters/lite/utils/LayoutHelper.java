6ff176e5d4799c15d0c83a6c89ce10061f3098c9
Vologhat <47892605+Vologhat@users.noreply.github.com>
2022-07-14 20:08:52 +0300

    added layout helper

---
+++ app/src/main/java/ru/vtosters/lite/utils/LayoutHelper.java
@@ -0,0 +1,24 @@
+package ru.vtosters.lite.utils;
+
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class LayoutHelper {
+
+    public static int MATCH_PARENT = -1;
+    public static int FILL_PARENT = -1;
+    public static int WRAP_CONTENT = -2;
+
+    public static LinearLayout.LayoutParams createLinear(int w, int h) {
+        return new LinearLayout.LayoutParams(w, h);
+    }
+
+    public static RelativeLayout.LayoutParams createRelative(int w, int h) {
+        return new RelativeLayout.LayoutParams(w, h);
+    }
+
+    public static FrameLayout.LayoutParams createFrame(int w, int h) {
+        return new FrameLayout.LayoutParams(w, h);
+    }
+}
