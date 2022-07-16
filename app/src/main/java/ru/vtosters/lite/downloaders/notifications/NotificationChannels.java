da5ac90189668d74980906d116944de120380741
aliveoutside <53598473+aliveoutside@users.noreply.github.com>
2022-07-16 19:43:16 +0300

    [musicdownloader] fix previous commit

---
+++ app/src/main/java/ru/vtosters/lite/downloaders/notifications/NotificationChannels.java
@@ -0,0 +1,32 @@
+package ru.vtosters.lite.downloaders.notifications;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationManagerCompat;
+
+import ru.vtosters.lite.utils.Globals;
+
+public class NotificationChannels {
+    public static final String MUSIC_DOWNLOAD_CHANNEL_ID = "music_download_channel";
+    public static final String MUSIC_DOWNLOAD_CHANNEL_NAME = "Загрузка музыки";
+    public static final String MUSIC_DOWNLOAD_CHANNEL_DESCRIPTION = "Прогресс загрузки музыки";
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static void createChannels() {
+        createMusicDownloadChannel();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static void createMusicDownloadChannel() {
+        var musicDownloadChannel = new NotificationChannel(MUSIC_DOWNLOAD_CHANNEL_ID, MUSIC_DOWNLOAD_CHANNEL_NAME, NotificationManager.IMPORTANCE_LOW);
+        musicDownloadChannel.setDescription(MUSIC_DOWNLOAD_CHANNEL_DESCRIPTION);
+        musicDownloadChannel.setShowBadge(true);
+        musicDownloadChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        var notificationManager = NotificationManagerCompat.from(Globals.getContext());
+        notificationManager.createNotificationChannel(musicDownloadChannel);
+    }
+}
