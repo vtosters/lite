252d761b9ee9281974ed97bf2481bea92defbd1a
aliveoutside <53598473+aliveoutside@users.noreply.github.com>
2022-07-28 17:07:06 +0300

    [accents] hook VKTabLayout instead of TabLayout

--- app/src/main/java/ru/vtosters/lite/themes/hooks/TabLayoutHook.java
+++ app/src/main/java/ru/vtosters/lite/themes/hooks/TabLayoutHook.java
@@ -3,15 +3,15 @@ package ru.vtosters.lite.themes.hooks;
 import android.content.res.ColorStateList;
 import android.view.View;
 
-import com.google.android.material.tabs.TabLayout;
+import com.vk.core.view.VKTabLayout;
 
 import ru.vtosters.lite.utils.Themes;
 
 public class TabLayoutHook implements BaseHook {
     @Override
     public void inject(View view, int i, boolean z) {
-        if (view instanceof TabLayout) {
-            var tabLayout = (TabLayout) view;
+        if (view instanceof VKTabLayout) {
+            var tabLayout = (VKTabLayout) view;
             tabLayout.setSelectedTabIndicatorColor(Themes.getAccentColor());
             tabLayout.setTabRippleColor(ColorStateList.valueOf(Themes.getAccentColor()));
         }
