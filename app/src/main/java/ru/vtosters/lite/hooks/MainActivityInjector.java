cd15541583978763aef7785d7fd277f1833bda9f
aliveoutside <53598473+aliveoutside@users.noreply.github.com>
2022-07-28 18:25:20 +0300

    [accents] fix attr injector

--- app/src/main/java/ru/vtosters/lite/hooks/MainActivityInjector.java
+++ app/src/main/java/ru/vtosters/lite/hooks/MainActivityInjector.java
@@ -11,14 +11,15 @@ import android.os.Build;
 import android.preference.PreferenceManager;
 
 import ru.vtosters.lite.downloaders.notifications.NotificationChannels;
+import ru.vtosters.lite.themes.ThemesCore;
 import ru.vtosters.lite.ui.dialogs.DisableBattery;
 import ru.vtosters.lite.ui.dialogs.InstallGMS;
 import ru.vtosters.lite.ui.dialogs.OTADialog;
 import ru.vtosters.lite.ui.dialogs.ServerDialog;
 import ru.vtosters.lite.ui.dialogs.Start;
+import ru.vtosters.lite.utils.ThemesUtils;
 
 public class MainActivityInjector {
-
     public static void inject(Activity activity) {
         setNeededTheme(activity);
         ServerDialog.activity = activity;
@@ -35,7 +36,7 @@ public class MainActivityInjector {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannels.createChannels();
         }
-
+        ThemesCore.setThemedColors(ThemesUtils.getAccentColor());
         // VKIDProtection.alert(activity);
     }
 }
