8e260a9de40aba1791bc74a6656623f00c539c62
Vologhat <47892605+Vologhat@users.noreply.github.com>
2022-07-30 01:41:43 +0300

    fixed errors

---
+++ app/src/main/java/ru/vtosters/lite/ui/dialogs/Translate.java
@@ -0,0 +1,35 @@
+package ru.vtosters.lite.ui.dialogs;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import ru.vtosters.lite.translators.YandexTranslator;
+
+public class Translate {
+    public static void showTranslatedText(Context context, String text) {
+        new Thread(() -> {
+            try {
+                var translation = YandexTranslator.translate(text);
+                new Handler(Looper.getMainLooper()).post(() -> {
+                    showDialog(context, translation);
+                });
+            } catch (Exception e) {
+                Log.e("Translate", e + "");
+            }
+        }).start();
+    }
+
+    private static void showDialog(Context context, String text) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
+        alertDialog.setTitle("Переводчик");
+        alertDialog.setMessage(text);
+        alertDialog.setPositiveButton("Закрыть", (dialog, which) -> {
+            dialog.cancel();
+        });
+        var alert = alertDialog.create();
+        alert.show();
+    }
+}
