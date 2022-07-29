fdeaa05360aeb5c06c46a0555c4b68486b6e0ef6
Vologhat <47892605+Vologhat@users.noreply.github.com>
2022-07-29 23:34:06 +0300

    removed deepl and added yandex

---
+++ app/src/main/java/ru/vtosters/lite/translators/YandexTranslator.java
@@ -0,0 +1,48 @@
+package ru.vtosters.lite.translators;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Locale;
+import java.util.UUID;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+public class YandexTranslator {
+
+    private static OkHttpClient client = new OkHttpClient();
+
+    static String uuid = UUID.randomUUID().toString().replace("-", "");
+
+    public static String translate(String text) {
+        try {
+            var request = new Request.a()
+                    .b("https://translate.yandex.net/api/v1/tr.json/translate?id=" + uuid + "-0-0&srv=android")
+                    .a(RequestBody.a(MediaType.a("application/x-www-form-urlencoded"),
+                            "lang=" + Locale.getDefault().getLanguage() + "&text=" + URLEncoder.encode(text, "UTF-8")))
+                    .a();
+            var payload = client.a(request).execute().a().g();
+            var json = new JSONObject(payload);
+            if (!json.has("text") && json.has("message")) {
+                throw new IOException(json.getString("message"));
+            }
+            JSONArray array = json.getJSONArray("text");
+            StringBuilder sb = new StringBuilder();
+            int length = array.length();
+            for (int i = 0; i < length; i++) {
+                sb.append(array.getString(i));
+            }
+            return sb.toString();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
