a94af3a4db2b09cdf07a704caef0a429ee692882
gdlbo <41114720+gdlbo@users.noreply.github.com>
2022-07-30 12:43:19 +0300

    message hook

--- app/src/main/java/ru/vtosters/lite/hooks/MessagesHook.java
+++ app/src/main/java/ru/vtosters/lite/hooks/MessagesHook.java
@@ -4,10 +4,11 @@ import static ru.vtosters.lite.utils.Preferences.getBoolValue;
 
 import android.text.TextUtils;
 
+import org.json.JSONObject;
+
 import ru.vtosters.lite.translators.BaseTranslator;
 
 public class MessagesHook {
-
     public static String injectOwnText(String oldText) {
         var translator = BaseTranslator.getInstance();
         if (!TextUtils.isEmpty(oldText) && getBoolValue("autotranslate", false)) {
