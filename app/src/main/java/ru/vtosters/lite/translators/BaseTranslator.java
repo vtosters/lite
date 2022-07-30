924a34b458b8b83fbd2311921b163f422026b670
gdlbo <41114720+gdlbo@users.noreply.github.com>
2022-07-30 11:57:26 +0300

    code imp

--- app/src/main/java/ru/vtosters/lite/translators/BaseTranslator.java
+++ app/src/main/java/ru/vtosters/lite/translators/BaseTranslator.java
@@ -1,15 +1,28 @@
 package ru.vtosters.lite.translators;
 
+import static ru.vtosters.lite.utils.Preferences.*;
+
 import androidx.annotation.NonNull;
+
+import java.util.Locale;
 
 import ru.vtosters.lite.utils.AndroidUtils;
 
 public abstract class BaseTranslator {
-
     private static final int YANDEX_CODE = 0;
 
     @NonNull
     public abstract String translate(String text);
+
+    public static Object target(){
+        var lang = preferences.getString("lang_target", "system");
+        var systemLang = Locale.getDefault().getLanguage();
+
+        if (lang.isEmpty() || lang.equals(systemLang) || lang.equals("system"))
+            return systemLang;
+
+        return lang;
+    }
 
     public static BaseTranslator getInstance() {
         switch (AndroidUtils.getPreferences().getInt("translator", 0)) {
