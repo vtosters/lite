5c79dbe1c2ef1234f5777061fc4c6c31ceae73c8
gdlbo <41114720+gdlbo@users.noreply.github.com>
2022-07-31 22:26:36 +0300

    req dumper

---
+++ app/src/main/java/com/vk/api/gifts/GiftGetByStickerId.java
@@ -0,0 +1,35 @@
+package com.vk.api.gifts;
+
+import android.content.Context;
+
+import com.vk.api.base.ApiRequest;
+import com.vk.dto.gift.CatalogedGift;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class GiftGetByStickerId extends ApiRequest<GiftGetByStickerId.a> {
+    public static class a {
+        public final int a;
+        public final CatalogedGift b;
+
+        public a(int i, CatalogedGift catalogedGift) {
+            this.a = i;
+            this.b = catalogedGift;
+        }
+    }
+
+    public GiftGetByStickerId(Context context, int i) {
+        super("execute.getGiftByStickerId");
+        b("sticker_id", i);
+        c("no_inapp", "0");
+        b("force_payment", 1);
+    }
+
+    @Override // com.vk.api.sdk.o.VKRequest
+    public GiftGetByStickerId.a a(JSONObject jSONObject) throws JSONException {
+        JSONObject jSONObject2 = jSONObject.getJSONObject("response");
+        return  new a(jSONObject2.optInt("balance"),
+                new CatalogedGift(jSONObject2.getJSONObject("gift")));
+    }
+}
