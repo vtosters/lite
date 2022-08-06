448a169bc3cd6be59720a9e7ec63eebc146d52ff
gdlbo <41114720+gdlbo@users.noreply.github.com>
2022-08-06 12:49:56 +0300

    code imp

--- app/src/main/java/ru/vtosters/lite/hooks/PicRoundingHook.java
+++ app/src/main/java/ru/vtosters/lite/hooks/PicRoundingHook.java
@@ -1,6 +1,12 @@
 package ru.vtosters.lite.hooks;
 import static ru.vtosters.lite.utils.Preferences.preferences;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
+
 import com.facebook.drawee.generic.RoundingParams;
 
 import ru.vtosters.lite.utils.AndroidUtils;
@@ -30,6 +36,20 @@ public class PicRoundingHook{
         }
 
         return new RoundingParams().a(orig, orig2, orig3, orig4);
+    }
+
+    public static void inject(Bitmap bitmap, Bitmap bitmap2, Paint b2) {
+        Shader.TileMode tileMode = Shader.TileMode.CLAMP;
+        b2.setShader(new BitmapShader(bitmap2, tileMode, tileMode));
+        Canvas canvas = new Canvas(bitmap);
+        float width = bitmap2.getWidth() / 2;
+
+        if (pref > 0) {
+            canvas.drawRoundRect(0, 0, bitmap.getWidth(), bitmap.getHeight(), AndroidUtils.dp2px(pref), AndroidUtils.dp2px(pref), b2);
+            return;
+        }
+
+        canvas.drawCircle(width, width, width, b2);
     }
 
     private static RoundingParams customParams(){
