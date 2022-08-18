341a368656508472481af99591578322385c4da8
spiky-s <49979126+spiky-s@users.noreply.github.com>
2022-08-18 20:22:37 +0300

    improved online hooks

--- app/src/main/java/ru/vtosters/lite/hooks/JsonInjectors.java
+++ app/src/main/java/ru/vtosters/lite/hooks/JsonInjectors.java
@@ -1,6 +1,8 @@
 package ru.vtosters.lite.hooks;
 
 import static ru.vtosters.lite.foaf.FoafBase.getLastSeen;
+import static ru.vtosters.lite.foaf.FoafBase.getBypassedOnlineInfo;
+import static ru.vtosters.lite.hooks.OnlineFormatterHook.onlineHookProfiles;
 import static ru.vtosters.lite.hooks.DateHook.getLocale;
 import static ru.vtosters.lite.utils.AndroidUtils.getDefaultPrefs;
 import static ru.vtosters.lite.utils.Base64Utils.decode;
@@ -271,28 +273,81 @@ public class JsonInjectors {
 
     public static JSONObject setOnlineInfo(JSONObject json) throws ParseException, IOException, JSONException {
         var id = json.optInt("id");
+        if (id == AccountManagerUtils.getUserId()) {
+            return json;
+        }
         var onlineinfo = json.optJSONObject("online_info");
-        var time = getLastSeen(0L, id);
+        if (onlineinfo != null && !onlineinfo.optBoolean("visible")) {
+            var bypassed = getBypassedOnlineInfo(id);
+            if (bypassed.optInt("last_seen", 0) != 0) {
+                json.remove("online_info");
 
-        if (time != 0L && onlineinfo != null && !onlineinfo.optBoolean("is_online") && id != AccountManagerUtils.getUserId()) {
-            json.remove("online_info");
+                var online_info = new JSONObject()
+                        .put("visible", true)
+                        .put("last_seen", bypassed.optInt("last_seen"))
+                        .put("is_online", bypassed.optBoolean("is_online"))
+                        .put("app_id", bypassed.optInt("app_id"))
+                        .put("is_mobile", bypassed.optBoolean("is_mobile"));
 
-            var online_info = new JSONObject()
-                    .put("visible", true)
-                    .put("last_seen", time)
-                    .put("is_online", false)
-                    .put("app_id", 0)
-                    .put("is_mobile", false);
+                var last_seen = new JSONObject()
+                        .put("platform", bypassed.optInt("platform"))
+                        .put("time", bypassed.optInt("last_seen"));
 
-            var last_seen = new JSONObject()
-                    .put("platform", 4)
-                    .put("time", time);
-
-            json.put("last_seen", last_seen).put("online_info", online_info);
+                json.put("last_seen", last_seen).put("online_info", online_info);
+            }
         }
 
         return json;
     }
+
+    public static JSONArray setOnlineInfoUsers(JSONArray profiles) throws ParseException, IOException, JSONException {
+        if (profiles == null || profiles.length() == 0) return profiles;
+        StringBuilder sb = new StringBuilder();
+        var curVkId = AccountManagerUtils.getUserId();
+        for (int i=0; i < profiles.length(); i++) {
+            JSONObject profile = profiles.getJSONObject(i);
+            int id = profile.optInt("id", -1);
+            JSONObject onlinfo = profile.optJSONObject("online_info");
+            if (id == curVkId || id < 0 || onlinfo == null || onlinfo.optBoolean("visible")) {
+                continue;
+            }
+            sb.append(id);
+            sb.append(",");
+        }
+        var ids = sb.toString();
+        if (ids.length() > 0) {
+            ids = ids.substring(0, ids.length() - 1);
+        } else {
+            return profiles;
+        }
+
+        JSONObject bypassedObj = getBypassedOnlineInfo(ids);
+        for (int i=0; i < profiles.length(); i++) {
+            JSONObject profile = profiles.getJSONObject(i);
+            int id = profile.optInt("id");
+            JSONObject bypassed = bypassedObj.optJSONObject(Integer.toString(id));
+            if (bypassed == null) {
+                continue;
+            }
+            profile.remove("online_info");
+
+            var online_info = new JSONObject()
+                    .put("visible", true)
+                    .put("last_seen", bypassed.optInt("last_seen"))
+                    .put("is_online", bypassed.optBoolean("is_online"))
+                    .put("app_id", bypassed.optInt("app_id"))
+                    .put("is_mobile", bypassed.optBoolean("is_mobile"));
+
+            var last_seen = new JSONObject()
+                    .put("platform", bypassed.optInt("platform"))
+                    .put("time", bypassed.optInt("last_seen"));
+
+            profile.put("last_seen", last_seen).put("online_info", online_info);
+        }
+
+        return profiles;
+    }
+
 
     public static JSONObject storiesads(JSONObject json, boolean isDeleteFix) throws JSONException {
         if (!adsstories()) {
@@ -612,8 +667,8 @@ public class JsonInjectors {
         return null;
     }
 
-    public static JSONObject friends(JSONObject json) throws JSONException {
-        JSONObject catalog = json;
+    public static JSONObject friends(JSONObject json) throws JSONException, ParseException, IOException {
+        JSONObject catalog = onlineHookProfiles(json);
         boolean sectionexecute = true;
         boolean hasBirthday = false;
 
