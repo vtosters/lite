2e8f168d9f3cf02369e8c978861fb96a728ba9f1
Erik Kiznov <46452865+k1zn@users.noreply.github.com>
2022-08-30 13:49:30 +0500

    Update Preferences.java

--- app/src/main/java/ru/vtosters/lite/utils/Preferences.java
+++ app/src/main/java/ru/vtosters/lite/utils/Preferences.java
@@ -21,6 +21,7 @@ import android.content.pm.PackageManager;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 
+import com.vk.core.concurrent.VkExecutors;
 import com.vtosters.lite.data.Users;
 import com.vtosters.lite.fragments.SettingsListFragment;
 
@@ -40,6 +41,11 @@ public class Preferences {
     } // VK Init
 
     public static void forceOffline() {
+        VkExecutors.x.q().a(() -> {
+            getInstance().autoCleaningCache();
+            setupFilters();
+        }); // slowTasksScheduler
+
         edit().putBoolean("isdark", isDarkTheme()).commit();
 
         if (setoffline() && offline()) {
