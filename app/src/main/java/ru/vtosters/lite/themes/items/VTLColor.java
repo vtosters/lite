810e5c72c1f87fbf264321631aeaebca8e01a466
Vologhat <dalvesrever@outlook.com>
2022-12-02 18:48:22 +0300

    add palettes

---
+++ app/src/main/java/ru/vtosters/lite/themes/items/VTLColor.java
@@ -0,0 +1,26 @@
+package ru.vtosters.lite.themes.items;
+
+public class VTLColor {
+
+    public String resName;
+    public int id;
+    public int color;
+
+    public VTLColor(int id, int color) {
+        this.id = id;
+        this.color = color;
+    }
+    public VTLColor(String resName, int id, int color) {
+        this.resName = resName;
+        this.id = id;
+        this.color = color;
+    }
+
+    public static VTLColor create(int id, int color) {
+        return new VTLColor(id, color);
+    }
+
+    public static VTLColor create(String resName, int id, int color) {
+        return new VTLColor(resName, id, color);
+    }
+}
