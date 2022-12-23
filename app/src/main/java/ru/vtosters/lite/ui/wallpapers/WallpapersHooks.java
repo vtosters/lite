118d86eb198d8e47906c6572e853d1f2e97adaee
saoq <wfmfe@ya.ru>
2022-12-24 01:54:00 +0300

    Simplify wallpapers handling

--- app/src/main/java/ru/vtosters/lite/ui/wallpapers/WallpapersHooks.java
+++ app/src/main/java/ru/vtosters/lite/ui/wallpapers/WallpapersHooks.java
@@ -1,80 +1,86 @@
 package ru.vtosters.lite.ui.wallpapers;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+import com.vtosters.lite.R;
+import ru.vtosters.lite.utils.AndroidUtils;
+import ru.vtosters.lite.utils.ThemesUtils;
+import vigo.sdk.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.function.Supplier;
+
 import static com.vk.im.engine.h.im_bg_chat;
 import static ru.vtosters.lite.ui.wallpapers.ImageFilters.getFilteredDrawable;
 import static ru.vtosters.lite.utils.AndroidUtils.getGlobalContext;
 import static ru.vtosters.lite.utils.AndroidUtils.sendToast;
 import static ru.vtosters.lite.utils.Preferences.getBoolValue;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.view.View;
-import android.widget.ImageView;
-
-import com.vtosters.lite.R;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-
-import ru.vtosters.lite.utils.AndroidUtils;
-import ru.vtosters.lite.utils.ThemesUtils;
-import vigo.sdk.Log;
-
 public class WallpapersHooks {
-    private static final boolean compress = getBoolValue("compresswp", true);
-    private static File mWallpaperFile;
+    private static final Supplier<Boolean> compress = () -> getBoolValue("compresswp", true);
+    private static final File originalWp = new File(getGlobalContext().getFilesDir(), "wallpaper.jpeg");
+    private static final File compressedWp = new File(getGlobalContext().getFilesDir(), "compressedwp.jpeg");
+    // TODO: cache to file
     private static Drawable mWallpaper;
     private static boolean mUpdateWallpaperRequested = true;
-    private static boolean mUpdateWallpaperFileRequested = true;
 
-    public static File getWallpaperFile() {
-        if (mWallpaperFile == null) {
-            mWallpaperFile = new File(getGlobalContext().getFilesDir(), "wallpaper.webp");
+    public static void setBg(View view) {
+        if (getWallpaper() != null) {
+            ((ImageView) view).setImageDrawable(getWallpaper()); // set picture to background
+        } else {
+            view.setBackgroundColor(ThemesUtils.getColorFromAttr(im_bg_chat)); // set default bg color
         }
-
-        return mWallpaperFile;
     }
 
-    public static Drawable getFilteredFile() {
-        File file = null;
-        File wp = mWallpaperFile;
-        String temp = getGlobalContext().getFilesDir() + File.separator + "filteredwp.webp";
-
-        if (wp != null && mUpdateWallpaperFileRequested) {
-            try {
-                file = new File(temp);
-                file.createNewFile();
-
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                ((BitmapDrawable) getFilteredDrawable(
-                        Drawable.createFromPath(wp.getAbsolutePath())))
-                        .getBitmap()
-                        .compress(Bitmap.CompressFormat.WEBP, (compress ? 80 : 95), bos);
-                byte[] bitmapdata = bos.toByteArray();
-
-                FileOutputStream fos = new FileOutputStream(file);
-                fos.write(bitmapdata);
-                fos.flush();
-                fos.close();
-
-            } catch (Exception e) {
-                Log.d("Wallpapers", e.getMessage());
-            }
-
-            mUpdateWallpaperFileRequested = false;
-        }
-
-        try {
-            return Drawable.createFromPath(temp);
-        } catch (Exception e) {
-            if (file != null) {
-                return Drawable.createFromPath(file.getAbsolutePath());
-            } else {
+    public static Drawable getWallpaper() {
+        if (mWallpaper == null || mUpdateWallpaperRequested) {
+            if (!originalWp.exists()) {
                 return null;
             }
+            Drawable drawable;
+            if (compress.get()) {
+                if (!compressedWp.exists() && !prepareCompressed()) {
+                    return null;
+                }
+                drawable = Drawable.createFromPath(compressedWp.getAbsolutePath());
+            } else {
+                drawable = Drawable.createFromPath(originalWp.getAbsolutePath());
+            }
+
+            if (drawable == null) {
+                return null;
+            }
+            mWallpaper = getFilteredDrawable(drawable);
+            mUpdateWallpaperRequested = false;
         }
+
+        return mWallpaper;
+    }
+
+    private static boolean prepareCompressed() {
+        if (!eligibleWallpaperFile(originalWp)) {
+            return false;
+        }
+        try (var f = new FileOutputStream(compressedWp)) {
+            var bitmap = BitmapFactory.decodeFile(originalWp.getAbsolutePath());
+            bitmap = resize(bitmap, 1080, 1920);
+            var drawable = new BitmapDrawable(null, bitmap);
+            drawable.getBitmap().compress(Bitmap.CompressFormat.WEBP, 90, f);
+        } catch (Exception e) {
+            Log.d("Wallpapers", e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+
+    public static File getWallpaperFile() {
+        return originalWp;
     }
 
     private static Boolean eligibleWallpaperFile(File file) {
@@ -87,61 +93,37 @@ public class WallpapersHooks {
         }
     }
 
-    public static void setBg(View view) {
-        if (getWallpaper() != null) {
-            ((ImageView) view).setImageDrawable(getWallpaper()); // set picture to background
-        } else {
-            view.setBackgroundColor(ThemesUtils.getColorFromAttr(im_bg_chat)); // set default bg color
-        }
-    }
-
-    public static String getWallpaperUrl() {
-        var file = getWallpaperFile();
-
-        if (file.exists() && eligibleWallpaperFile(file)) {
-            return file.getAbsolutePath();
-        }
-
-        return "default";
-    }
-
-    public static Drawable getWallpaper() {
-        if (mWallpaper == null || mUpdateWallpaperRequested) {
-            String url = getWallpaperUrl();
-
-            if (url.equals("default") || url.isEmpty()) {
-                return null;
-            }
-
-            Drawable drawable = Drawable.createFromPath(getWallpaperUrl());
-
-            if (drawable == null) {
-                return null;
-            }
-
-            mWallpaper = drawable;
-
-            mWallpaper = getFilteredDrawable(mWallpaper);
-
-            mUpdateWallpaperRequested = false;
-        }
-
-        return mWallpaper;
-    }
-
     public static void requestUpdateWallpaper() {
         mUpdateWallpaperRequested = true;
-        mUpdateWallpaperFileRequested = true;
     }
 
 
     public static void removeWallpaper() {
         try {
-            new File(getGlobalContext().getFilesDir(), "wallpaper.webp").delete();
-            new File(getGlobalContext().getFilesDir(), "filteredwp.webp").delete();
-            mWallpaperFile = null;
+            mWallpaper = null;
+            originalWp.delete();
+            compressedWp.delete();
         } catch (Exception e) {
             Log.d("Wallpapers", e.getMessage());
         }
+    }
+
+    private static Bitmap resize(Bitmap image, int maxWidth, int maxHeight) {
+        if (maxHeight > 0 && maxWidth > 0) {
+            int width = image.getWidth();
+            int height = image.getHeight();
+            float ratioBitmap = (float) width / (float) height;
+            float ratioMax = (float) maxWidth / (float) maxHeight;
+
+            int finalWidth = maxWidth;
+            int finalHeight = maxHeight;
+            if (ratioMax > ratioBitmap) {
+                finalWidth = (int) ((float) maxHeight * ratioBitmap);
+            } else {
+                finalHeight = (int) ((float) maxWidth / ratioBitmap);
+            }
+            image = Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true);
+        }
+        return image;
     }
 }
