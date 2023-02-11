c03dfd24eb514992d80caa6126a10a91d38c1604
saoq <wfmfe@ya.ru>
2023-02-11 16:30:02 +0300

    Forward calls from msg to off VK apps

--- app/src/main/java/ru/vtosters/lite/hooks/CallsHook.java
+++ app/src/main/java/ru/vtosters/lite/hooks/CallsHook.java
@@ -13,6 +13,7 @@ import ru.vtosters.lite.utils.AndroidUtils;
 import ru.vtosters.lite.utils.LifecycleUtils;
 
 import static android.widget.Toast.LENGTH_SHORT;
+import static ru.vtosters.lite.utils.AndroidUtils.getGlobalContext;
 
 public class CallsHook {
     public static void forwardToVkOffApps(View view, ExtendedUserProfile p) {
@@ -21,6 +22,10 @@ public class CallsHook {
 
     public static void forwardToVkOffApps(View view, Dialog dl) {
         call(view.getContext(), dl.getId());
+    }
+
+    public static void forwardToVkOffApps(Context ctx, int userId) {
+        call(ctx, userId);
     }
 
     public static void call(Context ctx, int id) {
@@ -33,9 +38,13 @@ public class CallsHook {
             Toast.makeText(LifecycleUtils.getCurrentActivity(), AndroidUtils.getString("calls_not_available"), LENGTH_SHORT).show();
 
             try {
-                ctx.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.vkontakte.android")));
+                var action = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.vkontakte.android"));
+                action.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                ctx.startActivity(action);
             } catch (android.content.ActivityNotFoundException e) {
-                ctx.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.vkontakte.android")));
+                var action = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.vkontakte.android"));
+                action.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                ctx.startActivity(action);
             }
 
             return;
@@ -43,12 +52,13 @@ public class CallsHook {
 
         var link = "https://vk.com/call?id=" + id;
         var startCall = new Intent(Intent.ACTION_VIEW, Uri.parse(link)).setPackage(vk ? "com.vkontakte.android" : "com.vk.im");
+        startCall.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         ctx.startActivity(startCall);
     }
 
     private static Boolean checkPackage(String packageName) {
         try {
-            var pm = AndroidUtils.getGlobalContext().getPackageManager();
+            var pm = getGlobalContext().getPackageManager();
             return pm.getApplicationInfo(packageName, 0).enabled;
         } catch (PackageManager.NameNotFoundException e) {
             return false;
