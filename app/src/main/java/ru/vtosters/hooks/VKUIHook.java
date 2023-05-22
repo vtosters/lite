d2c9c543a18b7ad6e53a7d4033c1b9561f0f5793
gdlbo <41114720+gdlbo@users.noreply.github.com>
2023-05-22 15:49:58 +0300

    refactored VKUIHook
    also fixed duplication of eruda

--- app/src/main/java/ru/vtosters/hooks/VKUIHook.java
+++ app/src/main/java/ru/vtosters/hooks/VKUIHook.java
@@ -11,29 +11,39 @@ import static ru.vtosters.hooks.other.Preferences.getBoolValue;
 
 public class VKUIHook {
     public static void inject(WebView webView) {
+        debugWebView(webView);
+        applyVKUIStyles(webView);
+    }
+
+    private static void debugWebView(WebView webView) {
         if (Preferences.getBoolValue("__dbg_webview", false)) {
             WebView.setWebContentsDebuggingEnabled(true);
-            webView.loadUrl("javascript:(function() {let parent = document.head || document.documentElement; let script = parent.appendChild(document.createElement('script')); script.src = 'https://cdn.jsdelivr.net/npm/eruda'; script.onload = () => eruda.init();})()");
+            webView.evaluateJavascript("if (!window.eruda) {let parent = document.head || document.documentElement; let script = parent.appendChild(document.createElement('script')); script.src = 'https://cdn.jsdelivr.net/npm/eruda'; script.onload = () => eruda.init();}", null);
         }
+    }
 
+    private static void applyVKUIStyles(WebView webView) {
         if (getBoolValue("VKUI_INJ", true)) {
             if (!WebViewColoringUtils.isLoaded) {
                 WebViewColoringUtils.load();
             }
-
-            StringBuilder sb = new StringBuilder();
-
-            if (ThemesCore.isCachedAccents()) {
-                sb.append("\n\n");
-                sb.append(WebViewColoringUtils.loadedCSS);
-            }
-
-            if (ThemesUtils.isAmoledTheme()) {
-                sb.append("\n\n");
-                sb.append(WebViewColoringUtils.loadedCSSAmoled);
-            }
-
-            WebViewColoringUtils.inject(webView, encodeToString(sb.toString().getBytes(), 2));
+            loadAndApplyCSS(webView);
         }
+    }
+
+    private static void loadAndApplyCSS(WebView webView) {
+        StringBuilder sb = new StringBuilder();
+
+        if (ThemesCore.isCachedAccents()) {
+            sb.append("\n\n");
+            sb.append(WebViewColoringUtils.loadedCSS);
+        }
+
+        if (ThemesUtils.isAmoledTheme()) {
+            sb.append("\n\n");
+            sb.append(WebViewColoringUtils.loadedCSSAmoled);
+        }
+
+        WebViewColoringUtils.inject(webView, encodeToString(sb.toString().getBytes(), 2));
     }
 }
