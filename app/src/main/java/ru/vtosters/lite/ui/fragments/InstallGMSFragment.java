aac86a99527463f27a2102fdd04e8e37afb5f1bc
gdlbo <41114720+gdlbo@users.noreply.github.com>
2023-09-28 11:09:40 +0300

    Update InstallGMSFragment.java
    added inotia00 microg information

--- app/src/main/java/ru/vtosters/lite/ui/fragments/InstallGMSFragment.java
+++ app/src/main/java/ru/vtosters/lite/ui/fragments/InstallGMSFragment.java
@@ -13,13 +13,15 @@ public class InstallGMSFragment extends TrackedMaterialPreferenceToolbarFragment
         addPreferencesFromResource(R.xml.preferences_gms);
 
         findPreference("microg").setOnPreferenceClickListener((preference) -> {
-            String[] items = {"Уменьшенный MicroG (Vanced MicroG)", "Полноценный MicroG (Замена GMS)"};
+            String[] items = {"Уменьшенный MicroG (ReVanced)", "Альтернативный MicroG (ReVanced)", "Полноценный MicroG (Замена GMS)"};
 
             new VkAlertDialog.Builder(requireActivity()).setItems(items, (dialog, which) -> {
                 switch (which) {
                     case 0 ->
                             requireContext().startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://github.com/TeamVanced/VancedMicroG/releases/latest/download/microg.apk")));
                     case 1 ->
+                            requireContext().startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://github.com/inotia00/mMicroG/releases/latest/download/microg.apk")));
+                    case 2 ->
                             requireContext().startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://github.com/microg/GmsCore/releases")));
                 }
                 dismiss();
