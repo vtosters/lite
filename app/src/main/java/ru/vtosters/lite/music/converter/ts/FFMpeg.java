86e624d991f5eeb65c4304e504bf7c8eed1b6e97
mexanoz <mexanoz@proton.me>
2023-10-25 19:09:08 +0600

    fix setting metadata for certain tracks

--- app/src/main/java/ru/vtosters/lite/music/converter/ts/FFMpeg.java
+++ app/src/main/java/ru/vtosters/lite/music/converter/ts/FFMpeg.java
@@ -33,9 +33,14 @@ public class FFMpeg {
     }
 
     private static String setMetadata(MusicTrack track) {
-        return Preferences.getBoolValue("setMetaData", false) ? " -metadata title=\"" + IOUtils.getValidFileName(M3UDownloader.getTitle(track)) + '"'
-                + " -metadata artist=\"" + normalizeMetadata(track.L.stream().map(Artist::w1).collect(Collectors.joining(", "))) + '"'
-                + " -metadata album=\"" + normalizeMetadata(track.I.getTitle()) + '"' : "";
+        if (!Preferences.getBoolValue("setMetaData", false)) {
+            return "";
+        }
+        return " -metadata title=\"" + IOUtils.getValidFileName(M3UDownloader.getTitle(track)) + '"'
+                + (track.L == null || track.I == null
+                ? " -metadata artist=\"" + normalizeMetadata(track.C) + '"'
+                : " -metadata artist=\"" + normalizeMetadata(track.L.stream().map(Artist::w1).collect(Collectors.joining(", "))) + '"'
+                + " -metadata album=\"" + normalizeMetadata(track.I.getTitle()) + '"');
     }
 
     private static String normalizeMetadata(String in) {
