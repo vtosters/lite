308b61bf002a1aba6add22d9b110dfe663b08fbb
gdlbo <41114720+gdlbo@users.noreply.github.com>
2024-02-02 20:41:56 +0300

    filters update system

--- app/src/main/java/ru/vtosters/sponsorpost/utils/Updates.java
+++ app/src/main/java/ru/vtosters/sponsorpost/utils/Updates.java
@@ -1,6 +1,7 @@
 package ru.vtosters.sponsorpost.utils;
 
 import ru.vtosters.hooks.other.Preferences;
+import ru.vtosters.lite.utils.NetworkUtils;
 import ru.vtosters.sponsorpost.data.Filter;
 import ru.vtosters.sponsorpost.data.Post;
 import ru.vtosters.sponsorpost.services.FilterService;
@@ -8,6 +9,7 @@ import ru.vtosters.sponsorpost.services.PostService;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 public class Updates {
     public static void updateFilters() {
@@ -18,27 +20,47 @@ public class Updates {
         }
 
         List<Filter> filters = FilterService.getFilters(savedFilterIds);
+        List<Filter> localFilters = FiltersPreferences.getAllDownloadedFilters();
 
         if (!filters.isEmpty()) {
-            FiltersPreferences.clearAll();
+            FiltersPreferences.clearAll(); // removes deleted filters from server
 
             for (Filter filter : filters) {
-                FiltersPreferences.saveFilter(filter);
+                Filter localFilter = findLocalFilterById(localFilters, filter.getId());
+                if (localFilter == null) {
+                    FiltersPreferences.saveFilter(filter);
+                } else {
+                    String remoteVersion = filter.getVersion();
+                    String localVersion = localFilter.getVersion();
+                    if (Objects.equals(remoteVersion, localVersion)) {
+                        FiltersPreferences.saveFilter(filter);
+                    } else {
+                        FiltersPreferences.saveFilter(localFilter);
+                    }
+                }
             }
         }
+    }
+
+    private static Filter findLocalFilterById(List<Filter> localFilters, int id) {
+        for (Filter filter : localFilters) {
+            if (filter.getId() == id) {
+                return filter;
+            }
+        }
+        return null;
     }
 
     public static void updatePosts() {
         long neededPosts = LocalDate.now().minusWeeks(1).toEpochDay();
 
-        if (!PostsPreferences.isEnabled() || Preferences.serverFeaturesDisable()) {
+        if (!PostsPreferences.isEnabled() || Preferences.serverFeaturesDisable() || !NetworkUtils.isNetworkConnected()) {
             return;
         }
 
         List<Post> posts = PostService.getPosts(neededPosts);
 
         if (!posts.isEmpty()) {
-            PostsPreferences.clearAll();
             PostsPreferences.savePosts(posts);
         }
     }
