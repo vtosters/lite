7c2590bc50788c333f51669c7dd341263f658e21
gdlbo <41114720+gdlbo@users.noreply.github.com>
2024-03-02 15:09:07 +0300

    added support for proxy api for another req

--- app/src/main/java/ru/vtosters/lite/downloaders/VideoDownloader.java
+++ app/src/main/java/ru/vtosters/lite/downloaders/VideoDownloader.java
@@ -13,68 +13,42 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.vk.core.dialogs.alert.VkAlertDialog;
 import com.vk.core.dialogs.bottomsheet.MenuBottomSheetAction;
+import com.vk.core.network.Network;
 import com.vk.core.util.ToastUtils;
 import com.vk.dto.common.VideoFile;
 import com.vk.dto.stories.model.StoryEntry;
 import com.vtosters.lite.R;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import ru.vtosters.lite.utils.LifecycleUtils;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.vk.core.network.Network.ClientType.CLIENT_API;
 import static ru.vtosters.hooks.other.ThemesUtils.getTextAttr;
-import static ru.vtosters.lite.net.Request.makeRequest;
 import static ru.vtosters.lite.proxy.ProxyUtils.getApi;
 import static ru.vtosters.lite.utils.AccountManagerUtils.getUserToken;
 
 public class VideoDownloader {
-    public static final int DOWNLOAD_ID = 0;
-    public static final int OPEN_EXTERNAL_LINK_ID = 1;
+    public static int DOWNLOAD_ID = 0;
+    public static int OPEN_EXTERNAL_LINK_ID = 1;
 
     public static void addAction(List<MenuBottomSheetAction> actions, int... ints) {
         actions.add(new MenuBottomSheetAction(ints[0], ints[1], ints[2], ints[3]));
     }
 
     public static void downloadVideo(VideoFile videoFile, Context context) {
-        final List<String> qualities = new ArrayList<>();
-        final List<String> urls = new ArrayList<>();
+        List<String> qualities = new ArrayList<>();
+        List<String> urls = new ArrayList<>();
 
-        if (!TextUtils.isEmpty(videoFile.D)) {
-            qualities.add("2160p");
-            urls.add(videoFile.D);
-        }
-
-        if (!TextUtils.isEmpty(videoFile.C)) {
-            qualities.add("1440p");
-            urls.add(videoFile.C);
-        }
-
-        if (!TextUtils.isEmpty(videoFile.B)) {
-            qualities.add("1080p");
-            urls.add(videoFile.B);
-        }
-
-        if (!TextUtils.isEmpty(videoFile.h)) {
-            qualities.add("720p");
-            urls.add(videoFile.h);
-        }
-
-        if (!TextUtils.isEmpty(videoFile.g)) {
-            qualities.add("480p");
-            urls.add(videoFile.g);
-        }
-
-        if (!TextUtils.isEmpty(videoFile.f)) {
-            qualities.add("360p");
-            urls.add(videoFile.f);
-        }
-        if (!TextUtils.isEmpty(videoFile.e)) {
-            qualities.add("240p");
-            urls.add(videoFile.e);
-        }
+        addVideos(videoFile, qualities, urls);
 
         Context finalContext = context != null ? context : LifecycleUtils.getCurrentActivity();
 
@@ -111,6 +85,42 @@ public class VideoDownloader {
                 .show();
     }
 
+    public static void addVideos(VideoFile videoFile, List<String> qualities, List<String> urls) {
+        if (!TextUtils.isEmpty(videoFile.D)) {
+            qualities.add("2160p");
+            urls.add(videoFile.D);
+        }
+
+        if (!TextUtils.isEmpty(videoFile.C)) {
+            qualities.add("1440p");
+            urls.add(videoFile.C);
+        }
+
+        if (!TextUtils.isEmpty(videoFile.B)) {
+            qualities.add("1080p");
+            urls.add(videoFile.B);
+        }
+
+        if (!TextUtils.isEmpty(videoFile.h)) {
+            qualities.add("720p");
+            urls.add(videoFile.h);
+        }
+
+        if (!TextUtils.isEmpty(videoFile.g)) {
+            qualities.add("480p");
+            urls.add(videoFile.g);
+        }
+
+        if (!TextUtils.isEmpty(videoFile.f)) {
+            qualities.add("360p");
+            urls.add(videoFile.f);
+        }
+        if (!TextUtils.isEmpty(videoFile.e)) {
+            qualities.add("240p");
+            urls.add(videoFile.e);
+        }
+    }
+
     public static void parseVideoLink(String url, Context ctx) {
         if (url.startsWith("http://")) {
             url.replace("http://", "https://");
@@ -124,20 +134,34 @@ public class VideoDownloader {
             progressDialog.setMessage(ctx.getString(R.string.video_dl_progress));
             progressDialog.show();
 
-            makeRequest("https://" + getApi() + "/method/stories.getById?stories=" + storyId + "&v=5.99&access_token=" + getUserToken(),
-                    response -> {
-                        progressDialog.cancel();
+            var req = new Request.a()
+                    .b("https://" + getApi() + "/method/stories.getById?stories=" + storyId + "&v=5.99&access_token=" + getUserToken())
+                    .a();
+            
+            Network.b(CLIENT_API).a(req).a(new Callback() {
 
-                        try {
-                            JSONObject mainJson = new JSONObject(response);
-                            JSONObject responseJson = mainJson.getJSONObject("response").optJSONArray("items").optJSONObject(0);
-                            StoryEntry story = new StoryEntry(responseJson);
-                            StoryDownloader.downloadStory(story);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            Toast.makeText(ctx, ctx.getString(R.string.video_dl_error), Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                @Override
+                public void a(Call call, IOException e) {
+                    e.fillInStackTrace();
+                    Toast.makeText(ctx, ctx.getString(R.string.video_dl_error), Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void a(Call call, Response response) throws IOException {
+                    progressDialog.cancel();
+
+                    try {
+                        JSONObject mainJson = new JSONObject(response.a().g());
+                        JSONObject responseJson = mainJson.getJSONObject("response").optJSONArray("items").optJSONObject(0);
+                        StoryEntry story = new StoryEntry(responseJson);
+                        StoryDownloader.downloadStory(story);
+                    } catch (JSONException e) {
+                        e.fillInStackTrace();
+                        Toast.makeText(ctx, ctx.getString(R.string.video_dl_error), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+            
             return;
         }
 
@@ -149,25 +173,37 @@ public class VideoDownloader {
         String videoId = url.split("video")[1];
         String ownerId = videoId.split("_")[0];
 
-        final ProgressDialog progressDialog = new ProgressDialog(ctx);
+        ProgressDialog progressDialog = new ProgressDialog(ctx);
         progressDialog.setMessage(ctx.getString(R.string.video_dl_progress));
         progressDialog.show();
 
-        makeRequest("https://" + getApi() + "/method/video.get?owner_id=" + ownerId + "&videos=" + videoId + "&v=5.99&access_token=" + getUserToken(),
-                response -> {
-                    progressDialog.cancel();
+        var req = new Request.a()
+                .b("https://" + getApi() + "/method/video.get?owner_id=" + ownerId + "&videos=" + videoId + "&v=5.99&access_token=" + getUserToken())
+                .a();
 
-                    try {
-                        JSONObject mainJson = new JSONObject(response);
-                        JSONObject responseJson = mainJson.getJSONObject("response");
-                        JSONArray itemsJson = responseJson.getJSONArray("items");
+        Network.b(CLIENT_API).a(req).a(new Callback() {
+            @Override
+            public void a(Call call, IOException e) {
+                e.fillInStackTrace();
+                Toast.makeText(ctx, ctx.getString(R.string.video_dl_error), Toast.LENGTH_SHORT).show();
+            }
 
-                        VideoFile videoFile = new VideoFile(itemsJson.getJSONObject(0));
-                        downloadVideo(videoFile, ctx);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        Toast.makeText(ctx, ctx.getString(R.string.video_dl_error), Toast.LENGTH_SHORT).show();
-                    }
-                });
+            @Override
+            public void a(Call call, Response response) throws IOException {
+                progressDialog.cancel();
+
+                try {
+                    JSONObject mainJson = new JSONObject(response.a().g());
+                    JSONObject responseJson = mainJson.getJSONObject("response");
+                    JSONArray itemsJson = responseJson.getJSONArray("items");
+
+                    VideoFile videoFile = new VideoFile(itemsJson.getJSONObject(0));
+                    downloadVideo(videoFile, ctx);
+                } catch (JSONException e) {
+                    e.fillInStackTrace();
+                    Toast.makeText(ctx, ctx.getString(R.string.video_dl_error), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
