08dc8b1f21a5212f9e98a4446ad8b84321c68930
gdlbo <41114720+gdlbo@users.noreply.github.com>
2024-03-24 19:02:40 +0300

    fixes

--- app/src/main/java/com/vk/music/stats/VKMusicStatsTracker.java
+++ app/src/main/java/com/vk/music/stats/VKMusicStatsTracker.java
@@ -5,16 +5,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
 import androidx.core.app.NotificationCompat;
 import com.vk.core.extensions.StringExt;
+import com.vk.core.util.DeviceIdProvider;
 import com.vk.music.common.MusicPlaybackLaunchContext;
 import com.vk.music.logger.MusicLogger;
 import com.vk.music.stats.i.MusicPlaybackParams;
 import com.vk.navigation.NavigatorKeys;
 import com.vtosters.lite.data.Analytics;
 import kotlin.jvm.b.Functions2;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import ru.vtosters.hooks.other.Preferences;
 import ru.vtosters.lite.music.Metrics;
+import ru.vtosters.lite.utils.AndroidUtils;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -152,62 +155,60 @@ public final class VKMusicStatsTracker implements MusicStatsTracker {
     }
 
     private void makeMetricsRequest(MusicPlaybackParams musicPlaybackParams, String str) {
-        StringBuilder builder = new StringBuilder();
+        JSONObject eventObject = new JSONObject();
+        JSONArray eventsArray = new JSONArray();
 
-        builder.append("API.stats.trackEvents({events:");
-        builder.append("\"[{");
-        builder.append("\"event_name\":\"").append(str).append("\",");
-        builder.append("\"audio_id\":\"").append(musicPlaybackParams.c()).append("\",");
-        builder.append("\"uuid\":\"").append(UUID.randomUUID().hashCode()).append("\",");
-        builder.append("\"shuffle\":\"").append(musicPlaybackParams.l()).append("\",");
-        builder.append("\"reason\":\"").append(e(musicPlaybackParams)).append("\",");
-        builder.append("\"start_time\":\"").append(musicPlaybackParams.i()).append("\",");
-        builder.append("\"playback_started_at\":\"").append(musicPlaybackParams.d()).append("\",");
-        builder.append("\"state\":\"").append(musicPlaybackParams.m()).append("\",");
-        builder.append("\"track_code\":\"").append(musicPlaybackParams.j()).append("\",");
+        try {
+            eventObject.put("event_name", str);
+            eventObject.put("audio_id", musicPlaybackParams.c());
+            eventObject.put("uuid", UUID.randomUUID().hashCode());
+            eventObject.put("shuffle", musicPlaybackParams.l());
+            eventObject.put("reason", e(musicPlaybackParams));
+            eventObject.put("start_time", musicPlaybackParams.i());
+            eventObject.put("playback_started_at", musicPlaybackParams.d());
+            eventObject.put("state", musicPlaybackParams.m());
+            eventObject.put("track_code", musicPlaybackParams.j());
 
-        if (!Objects.equals("music_start_playback", str)) {
-            builder.append("\"duration\":\"").append(musicPlaybackParams.a()).append("\",");
+            if (!Objects.equals("music_start_playback", str)) {
+                eventObject.put("duration", musicPlaybackParams.a());
+            }
+
+            int repeatType = h.$EnumSwitchMapping$0[musicPlaybackParams.b().ordinal()];
+            switch (repeatType) {
+                case 1 -> eventObject.put("repeat", "one");
+                case 2 -> eventObject.put("repeat", "all");
+            }
+
+            MusicPlaybackLaunchContext h = musicPlaybackParams.h();
+
+            eventObject.put("source", h.v0());
+
+            if (h.v1()) {
+                eventObject.put("playlist_id", h.u1());
+            }
+
+            if (h.i(4) || h.i(8)) {
+                eventObject.put("expanded", h.i(4));
+            }
+
+            if (StringExt.a((CharSequence) musicPlaybackParams.e())) {
+                eventObject.put("prev_audio_id", musicPlaybackParams.e());
+            }
+
+            if (StringExt.a((CharSequence) musicPlaybackParams.f())) {
+                eventObject.put("prev_playlist_id", musicPlaybackParams.f());
+            }
+
+            eventsArray.put(eventObject);
+
+            String builder = String.format("API.stats.trackEvents(%s);", new JSONObject().put("events", eventsArray));
+
+            Metrics.trackEvents(builder);
+            Metrics.sendStartEvent(musicPlaybackParams.c(), DeviceIdProvider.d(AndroidUtils.getGlobalContext()));
+        } catch (JSONException e) {
+            e.fillInStackTrace();
         }
-
-        int repeatType = h.$EnumSwitchMapping$0[musicPlaybackParams.b().ordinal()];
-        switch (repeatType) {
-            case 1 -> builder.append("\"repeat\":\"one\",");
-            case 2 -> builder.append("\"repeat\":\"all\",");
-        }
-
-        MusicPlaybackLaunchContext h = musicPlaybackParams.h();
-
-        builder.append("\"source\":\"").append(h.v0()).append("\",");
-
-        if (h.v1()) {
-            builder.append("\"playlist_id\":\"").append(h.u1()).append("\",");
-        }
-
-        if (h.i(4) || h.i(8)) {
-            builder.append("\"expanded\":\"").append(h.i(4)).append("\",");
-        }
-
-        if (StringExt.a((CharSequence) musicPlaybackParams.e())) {
-            builder.append("\"prev_audio_id\":\"").append(musicPlaybackParams.e()).append("\",");
-        }
-
-        if (StringExt.a((CharSequence) musicPlaybackParams.f())) {
-            builder.append("\"prev_playlist_id\":\"").append(musicPlaybackParams.f()).append("\",");
-        }
-
-        if (builder.charAt(builder.length() - 1) == ',') {
-            builder.setCharAt(builder.length() - 1, '}');
-        } else {
-            builder.append('}');
-        }
-
-        builder.append("]\"");
-        builder.append("});");
-
-        Metrics.sendMetrics(builder.toString());
     }
-
 
     private Analytics.l a(MusicPlaybackParams musicPlaybackParams, String str) {
         Analytics.l invoke = this.b.invoke(str);
--- app/src/main/java/ru/vtosters/lite/music/Metrics.java
+++ app/src/main/java/ru/vtosters/lite/music/Metrics.java
@@ -2,32 +2,56 @@ package ru.vtosters.lite.music;
 
 import android.util.Log;
 import com.vk.core.network.Network;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import okhttp3.*;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import static com.vk.core.network.Network.ClientType.CLIENT_API;
 import static ru.vtosters.lite.proxy.ProxyUtils.getApi;
 import static ru.vtosters.lite.utils.AccountManagerUtils.getUserToken;
 
 public class Metrics {
-    public static void sendMetrics(String event) {
+    public static void trackEvents(String event) {
         try {
-            request(event);
-        } catch (JSONException e) {
+            trackEventsRequest(event);
+        } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void request(String event) throws JSONException {
+    public static void sendStartEvent(String audio, String uuid) {
+        sendStartEventRequest(audio, uuid);
+    }
+
+    private static void sendStartEventRequest(String audio, String uuid) {
         Request req = new Request.a()
-                .b("https://" + getApi() + "/method/execute?code=" + event + "&v=5.186&access_token=" + getUserToken())
+                .b("https://" + getApi() + "/method/audio.sendStartEvent?audio_id=" + audio +
+                        "&uuid=" + uuid +
+                        "&device_id=" + uuid +
+                        "&sak_version=1.115" +
+                        "&gaid=00000000-0000-0000-0000-000000000000" +
+                        "&v=5.186&access_token=" + getUserToken())
+                .a();
+
+        Network.b(CLIENT_API).a(req).a(new Callback() {
+            @Override
+            public void a(Call call, IOException e) {
+                e.fillInStackTrace();
+            }
+
+            @Override
+            public void a(Call call, Response response) throws IOException {
+                Log.d("MusicMetrics", response.a().g());
+            }
+        });
+    }
+
+    private static void trackEventsRequest(String event) throws UnsupportedEncodingException {
+        Request req = new Request.a()
+                .b("https://" + getApi() + "/method/execute?v=5.186&access_token=" + getUserToken())
+                .a(RequestBody.a(MediaType.a("application/x-www-form-urlencoded"), URLEncoder.encode(event, "UTF-8")))
                 .a();
 
         Network.b(CLIENT_API).a(req).a(new Callback() {
