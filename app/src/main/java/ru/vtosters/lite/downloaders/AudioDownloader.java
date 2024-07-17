fb23dd20049fb7c8a2925669d6807275b258c14a
Artur Skubei <41114720+gdlbo@users.noreply.github.com>
2024-07-17 13:57:04 +0300

    refactored AudioDownloader

--- app/src/main/java/ru/vtosters/lite/downloaders/AudioDownloader.java
+++ app/src/main/java/ru/vtosters/lite/downloaders/AudioDownloader.java
@@ -32,7 +32,6 @@ import ru.vtosters.lite.utils.music.PlaylistUtils;
 import java.io.File;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.Future;
 
 import static ru.vtosters.lite.utils.AndroidUtils.getString;
 
@@ -43,145 +42,86 @@ import static ru.vtosters.lite.utils.AndroidUtils.getString;
  */
 
 public class AudioDownloader {
-    public static final String dlpath = Preferences.getBoolValue("dldir", false) ? Environment.DIRECTORY_DOWNLOADS : Environment.DIRECTORY_MUSIC;
+    private static final String DOWNLOAD_PATH = Preferences.getBoolValue("dldir", false)
+            ? Environment.DIRECTORY_DOWNLOADS
+            : Environment.DIRECTORY_MUSIC;
 
-    public static Future<?> downloadPlaylist(Playlist playlist) {
-        return VTExecutors.getMusicDownloadExecutor().submit(() -> {
+    public static void downloadPlaylist(Playlist playlist) {
+        submitTask(() -> {
             List<MusicTrack> tracks = PlaylistConverter.getPlaylist(playlist);
-
             String playlistName = IOUtils.getValidFileName(playlist.g);
-
-            String musicPath = Environment.getExternalStoragePublicDirectory(dlpath).getAbsolutePath();
-            String downloadPath = musicPath + File.separator + playlistName;
-
+            String downloadPath = getDownloadPath(playlistName);
             int notificationId = playlistName.hashCode();
             NotificationCompat.Builder notification = MusicNotificationBuilder.buildPlaylistDownloadNotification(playlistName, notificationId);
 
-            PlaylistDownloader.downloadPlaylist(
-                    tracks,
-                    IOUtils.getValidFileName(playlist.g),
-                    downloadPath,
-                    MusicCallbackBuilder.buildPlaylistCallback(tracks.size(), notification, notificationId)
-            );
+            downloadPlaylist(tracks, playlistName, downloadPath, notification, notificationId);
         });
     }
 
-
-    public static Future<?> downloadAudio(MusicTrack track) {
-        return VTExecutors.getMusicDownloadExecutor().submit(() -> downloadM3U8(track, false));
+    public static void downloadAudio(MusicTrack track) {
+        submitTask(() -> downloadM3U8(track, false));
     }
 
-    public static Future<?> cacheTrack(MusicTrack track) {
-        return VTExecutors.getMusicDownloadExecutor().submit(() -> {
+    public static void cacheTrack(MusicTrack track) {
+        submitTask(() -> {
             String trackId = LibVKXClient.asId(track);
+            File trackFile = MusicCacheFilesHook.getTrackFile(trackId);
 
             if (MusicCacheImpl.isCachedTrack(trackId)) {
                 MusicCacheImpl.removeTrack(trackId);
                 AndroidUtils.sendToast(AndroidUtils.getString("audio_deleted_from_cache"));
             } else {
-                File trackFile = MusicCacheFilesHook.getTrackFile(trackId);
-                if (!trackFile.exists()) {
-                    trackFile.getParentFile().mkdirs();
-                }
+                ensureDirectoryExists(trackFile);
                 downloadM3U8(track, true);
             }
         });
     }
 
-    public static Future<?> cachePlaylist(Playlist playlist) {
-        return VTExecutors.getMusicDownloadExecutor().submit(() -> {
+    public static void cachePlaylist(Playlist playlist) {
+        submitTask(() -> {
             List<MusicTrack> tracks = PlaylistConverter.getPlaylist(playlist);
-
             int notificationId = playlist.g.hashCode();
             NotificationCompat.Builder notification = MusicNotificationBuilder.buildPlaylistDownloadNotification(playlist.g, notificationId);
 
-            JSONObject thumbs = PlaylistUtils.getThumb(playlist);
-
-            if (thumbs != null) {
-                new ThumbnailPlaylistDownloader(new Callback() {
-                    @Override public void onProgress(int progress) { }
-
-                    @Override
-                    public void onSuccess() {
-                        PlaylistCacheDbDelegate.addPlaylist(AndroidUtils.getGlobalContext(), playlist);
-
-                        Log.d("Playlist", "adding to cache with thumbs " + playlist.v1());
-                    }
-
-                    @Override
-                    public void onFailure(Throwable e) {
-                        throw new RuntimeException(e);
-                    }
-                }).download(playlist);
-            } else {
-                PlaylistCacheDbDelegate.addPlaylist(AndroidUtils.getGlobalContext(), playlist);
-
-                Log.d("Playlist", "adding to cache without thumbs " + playlist.a);
-            }
-
-            PlaylistDownloader.cachePlaylist(
-                    tracks,
-                    MusicCallbackBuilder.buildPlaylistCallback(tracks.size(), notification, notificationId),
-                    playlist
-            );
+            cachePlaylist(tracks, notification, notificationId, playlist);
         });
     }
 
-    public static Future<?> cacheAllAudios() {
-        return VTExecutors.getMusicDownloadExecutor().submit(() -> {
+    public static void cacheAllAudios() {
+        submitTask(() -> {
             List<MusicTrack> tracks = AudioGet.getAudios();
-
             int notificationId = AccountManagerUtils.getUserId();
             NotificationCompat.Builder notification = MusicNotificationBuilder.buildAllAudiosDownloadNotification(notificationId);
 
-            if (tracks.isEmpty()) {
-                return;
+            if (!tracks.isEmpty()) {
+                cachePlaylist(tracks, notification, notificationId, PlaylistHelper.createCachedPlaylistMetadata());
             }
-
-            PlaylistDownloader.cachePlaylist(
-                    tracks,
-                    MusicCallbackBuilder.buildPlaylistCallback(tracks.size(), notification, notificationId),
-                    PlaylistHelper.createCachedPlaylistMetadata()
-            );
         });
     }
 
-    public static Future<?> downloadAllAudios() {
-        return VTExecutors.getMusicDownloadExecutor().submit(() -> {
+    public static void downloadAllAudios() {
+        submitTask(() -> {
             List<MusicTrack> tracks = AudioGet.getAudios();
-
             int notificationId = AccountManagerUtils.getUserId();
             NotificationCompat.Builder notification = MusicNotificationBuilder.buildAllAudiosDownloadNotification(notificationId);
-
             String playlistName = "Audios of " + AccountManagerUtils.getUserId();
+            String downloadPath = getDownloadPath(playlistName);
 
-            String musicPath = Environment.getExternalStoragePublicDirectory(dlpath).getAbsolutePath();
-            String downloadPath = musicPath + File.separator + playlistName;
-
-            if (tracks.isEmpty()) {
-                return;
+            if (!tracks.isEmpty()) {
+                downloadPlaylist(tracks, playlistName, downloadPath, notification, notificationId);
             }
-
-            PlaylistDownloader.downloadPlaylist(
-                    tracks,
-                    playlistName,
-                    downloadPath,
-                    MusicCallbackBuilder.buildPlaylistCallback(tracks.size(), notification, notificationId)
-            );
         });
     }
 
-    private static Future<?> downloadM3U8(MusicTrack track, boolean cache) {
-        return VTExecutors.getMusicDownloadExecutor().submit(() -> {
+    private static void downloadM3U8(MusicTrack track, boolean cache) {
+        submitTask(() -> {
             Objects.requireNonNull(track.D, () -> {
                 ToastUtils.a(getString(R.string.link_audio_error));
                 return "link must not be null";
             });
-            String musicPath = Environment.getExternalStoragePublicDirectory(dlpath).getAbsolutePath();
             int tempId = track.d;
-            String downloadPath = musicPath + File.separator;
+            String downloadPath = Environment.getExternalStoragePublicDirectory(DOWNLOAD_PATH).getAbsolutePath() + File.separator;
             NotificationCompat.Builder notification = MusicNotificationBuilder.buildDownloadNotification(track, tempId);
-
             Callback cb = MusicCallbackBuilder.buildOneTrackCallback(tempId, notification);
 
             if (cache) {
@@ -190,5 +130,61 @@ public class AudioDownloader {
                 TrackDownloader.downloadTrack(track, downloadPath, cb);
             }
         });
+    }
+
+    private static void downloadPlaylist(List<MusicTrack> tracks, String playlistName, String downloadPath, NotificationCompat.Builder notification, int notificationId) {
+        PlaylistDownloader.downloadPlaylist(
+                tracks,
+                playlistName,
+                downloadPath,
+                MusicCallbackBuilder.buildPlaylistCallback(tracks.size(), notification, notificationId)
+        );
+    }
+
+    private static void cachePlaylist(List<MusicTrack> tracks, NotificationCompat.Builder notification, int notificationId, Playlist playlist) {
+        JSONObject thumbs = PlaylistUtils.getThumb(playlist);
+
+        if (thumbs != null) {
+            new ThumbnailPlaylistDownloader(new Callback() {
+                @Override
+                public void onProgress(int progress) {
+                }
+
+                @Override
+                public void onSuccess() {
+                    PlaylistCacheDbDelegate.addPlaylist(AndroidUtils.getGlobalContext(), playlist);
+                    Log.d("Playlist", "adding to cache with thumbs " + playlist.v1());
+                }
+
+                @Override
+                public void onFailure(Throwable e) {
+                    throw new RuntimeException(e);
+                }
+            }).download(playlist);
+        } else {
+            PlaylistCacheDbDelegate.addPlaylist(AndroidUtils.getGlobalContext(), playlist);
+            Log.d("Playlist", "adding to cache without thumbs " + playlist.a);
+        }
+
+        PlaylistDownloader.cachePlaylist(
+                tracks,
+                MusicCallbackBuilder.buildPlaylistCallback(tracks.size(), notification, notificationId),
+                playlist
+        );
+    }
+
+    private static String getDownloadPath(String playlistName) {
+        String musicPath = Environment.getExternalStoragePublicDirectory(DOWNLOAD_PATH).getAbsolutePath();
+        return musicPath + File.separator + playlistName;
+    }
+
+    private static void ensureDirectoryExists(File file) {
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+        }
+    }
+
+    private static void submitTask(Runnable task) {
+        VTExecutors.getMusicDownloadExecutor().submit(task);
     }
 }
